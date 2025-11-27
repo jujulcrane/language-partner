710f9cc5d46b1b25d4b0761b5eee7547cfab8788
Juliet Crane <jlc565@cornell.edu>
2025-11-26 20:53:08 -0500

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -24,4 +24,4 @@ app.use(ttsRouter); // /api/text-to-speech
 app.use(translateRouter); // /api/translate
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT} on all interfaces`));
